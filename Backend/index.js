652c7b7637162c49901b5c68815a6dcd9f098c12
Nitish Ojha <nitishojha00@gmail.com>
2026-01-26 18:32:54 +0530

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 // 1. FIX CORS: Add 5500 (Live Server default)
 app.use(cors({
-  origin: ["http://127.0.0.1:3000"], 
+  origin: ["http://127.0.0.1:3000","https://codolio-clone.vercel.app"], 
   credentials: true 
 }));
 
